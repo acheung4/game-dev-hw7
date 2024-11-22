cec94dd2ce151cfda2dcb39c51c158ee721cbc17
acheung4 <acheung4@villanova.edu>
2024-11-22 09:33:09 -0500

    Finish homework

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath() {
+        foreach(Waypoint waypoint in path) {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercentage = 0;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercentage < 1) {
+                travelPercentage += Time.deltaTime;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercentage);
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }
+    }
+}
